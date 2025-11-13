cd20ee376879462661c388bd199001f7e3a2d48c
Luis Henriique <luishenrique162004@gmail.com>
2025-11-13 20:44:39 -0300

    Tratando a lógica da região crítica

--- main.cpp
+++ main.cpp
@@ -28,46 +28,43 @@ void main_game_loop();
 // Global shared states
 const int LIN = 10;
 const int COL = 30;
-char map[LIN][COL] = {
-    "############################", // # = Parede do jogo
-    "#                          #", // C = Região Crítica (Passagem)
-    "########  #  ########  #####", // 1 = Jogador 1
-    "#      #  #  #      #  #   #", // 1 = Jogador 1
-    "#  #####  #  #      #  #   #", // F = Bandeira
-    "#         #  #      #  #   #",
-    "#  #######   ###############",
-    "#         CCCC            F#",
-    "############################"};
 char base_map[LIN][COL]; // Será a cópia do labirinto no estado atual, é uma camada fixa que contém os dados originais do mapa
+char map[LIN][COL] =
+    {
+        "############################", // # = Parede do jogo
+        "#                          #", // C = Região Crítica (Passagem)
+        "########  #  ########  #####", // 1 = Jogador 1
+        "#      #  #  #      #  #   #", // 1 = Jogador 1
+        "#  #####  #  #      #  #   #", // F = Bandeira
+        "#         #  #      #  #   #",
+        "#  #######   ###############",
+        "#         CCCC            F#",
+        "############################"};
 
 // Variáveis globais
-mutex mtx_map;       //  Evita condição de corrida ao desenhar o mapa
+mutex mtx_map;       //  Evita condição de corrida ao desenhar o mapa - mutex (Garante que apenas umas das 3 threads opere por vez)
 bool playing = true; // Jogo está rodando
-sem_t sem_RC;        // Região Crítica
+sem_t sem_RC;        // semáforo para  Região Crítica (RC)
 
 void draw_map()
-{
-    /*Desenha o mapa no terminal*/
-    system("clear");
+{ /*Função responsável por imprimir o mapa no terminal.*/
+
+    system("clear"); // Limpa tela do terminal
     for (int i = 0; i < LIN; i++)
     {
         cout << map[i] << endl;
     }
 }
 
-/*Verifica se é possível se mover para a próxima posição*/
 bool allow_move(int nx, int ny)
 {
+    /*Função responsável por verificar se é possível se mover para a próxima posição indicada.*/
     return (map[nx][ny] != '#' && nx >= 0 && ny >= 0 && nx < LIN && ny < COL);
 }
 
-// posições iniciais de cada jogador
-Player p1 = {1, 1, '1', ' '};
-Player p2 = {1, 26, '2', ' '};
-
 int kbhit()
 {
-    /*Função responsável por realizar as leituras das teclas no teclado nao bloqueante*/
+    /*Função responsável por realizar as leituras das teclas no teclado de maneira não bloqueante*/
     struct termios oldt, newt;
     int ch;
     int oldf;
@@ -88,9 +85,13 @@ int kbhit()
     return 0;
 }
 
+// posições iniciais de cada jogador
+Player p1 = {1, 1, '1', ' '};
+Player p2 = {1, 26, '2', ' '};
+
 void thread_player(Player &p)
 {
-    // Verifica se o usuário digitou algo no teclado e foi capturado p/ p.direction
+    // Verifica se o usuário digitou algo no teclado, com isso será capturado p/ p.direction
     // Coloca e verifica os movimentos, após isso zera o buffer de direção para evitar de mover sem parar
     while (playing)
     {
@@ -105,6 +106,8 @@ void thread_player(Player &p)
 
 void move_player(Player &p)
 {
+    /*Função responsável por movimentar o jodador, se atendando a região crítica.*/
+
     int nx = p.x, ny = p.y;
     switch (p.direction)
     {
@@ -124,38 +127,29 @@ void move_player(Player &p)
         return;
     }
 
+    // Verifica se o novo movimento é permitido
     if (!allow_move(nx, ny))
         return;
 
     // Garante a exclusão mútua no acesso ao mapa, utilizamos unique_lock pois podemos liberar o mutex e pegar novamente depois
     unique_lock<mutex> lock(mtx_map);
 
-    // evita sobreposição imediata
+    // evita sobreposição imediata entre os jogadores
     if (map[nx][ny] == '1' || map[nx][ny] == '2')
         return;
 
     // capture o que havia na célula anterior (base) para checar saída depois
     char prev_base = base_map[p.x][p.y];
-    // Retorna true se o player está fora da RC e vai entrar em seguida
-    bool enteringRC = (base_map[nx][ny] == 'C' && base_map[p.x][p.y] != 'C');
 
-    if (enteringRC)
+    // Verifica se o player está fora da RC e vai entrar no movimento seguinte
+    if (base_map[nx][ny] == 'C' && base_map[p.x][p.y] != 'C')
     {
-        // vamos entrar na RC: liberar mutex antes de esperar no semáforo
-        lock.unlock();
-        sem_wait(&sem_RC);
-        lock.lock();
-
-        // revalidar condições após ter adquirido semáforo
-        if (!allow_move(nx, ny) || map[nx][ny] == '1' || map[nx][ny] == '2')
-        {
-            // não é possível entrar (outro jogador ocupou), libera semáforo e sai
-            sem_post(&sem_RC);
-            return;
-        }
+        // Tenta entrar na RC, sem bloquear a thread caso RC esteja ocupada.
+        if (sem_trywait(&sem_RC) != 0)
+            return; // RC está ocupada, nao deixa entrar, mas ainda tem os movimentos livres, nao congela.
     }
 
-    // restaura a célula anterior ao seu valor de base (C, F ou ' ')
+    // restaura a célula anterior ao seu valor de base (C, F ou ' '), essa mudança é feita com o mutex travado
     map[p.x][p.y] = base_map[p.x][p.y];
 
     // atualiza posição do jogador
@@ -169,14 +163,12 @@ void move_player(Player &p)
     draw_map();
 
     // detecta saída da RC: se prev_base era 'C' e a base da atual não é 'C'
-    bool just_left_RC = (prev_base == 'C' && base_map[p.x][p.y] != 'C');
-
-    if (just_left_RC)
+    if (prev_base == 'C' && base_map[p.x][p.y] != 'C')
     {
         // liberamos semáforo - já não precisamos do mutex para isso,
         // mas como ainda temos lock, liberamos depois de postar
         // (não há risco de deadlock porque sem_post não bloqueia)
-        sem_post(&sem_RC);
+        sem_post(&sem_RC); // Incrementa valor do semáforo
     }
 
     // se chegou na bandeira (base == 'F'), encerra
@@ -227,7 +219,7 @@ void main_game_loop()
                 p2.direction = 'r';
                 break;
             case 'b':
-                // Tecla enter para encerrar o jogo
+                // Tecla 'b' para encerrar o jogo no brute_force
                 playing = false;
                 break;
             default:
@@ -248,7 +240,8 @@ int main()
             base_map[i][j] = map[i][j];
         }
     }
-    sem_init(&sem_RC, 0, 1); // apenas um jogador por vez
+
+    sem_init(&sem_RC, 0, 1); // apenas um jogador por vez pode ter acesso a região crítica
 
     // Coloca os jogadores nas posições inicias
     map[p1.x][p1.y] = '1';
